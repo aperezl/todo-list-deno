5ef23b99d4375a86e93ef206070a924c7324bbac
Antonio Manuel Perez Lopez <antonio.perez@podsystem.com>
2020-05-20 23:54:53 +0200

    Add .gitignore

--- db/mongodb.ts
+++ db/mongodb.ts
@@ -1,7 +1,16 @@
 import { config } from 'https://deno.land/x/dotenv/mod.ts'
-import { init, MongoClient } from 'https://deno.land/x/mongo/mod.ts'
+import { MongoClient } from 'https://deno.land/x/mongo/mod.ts'
+import { exists } from 'https://deno.land/std@0.51.0/fs/mod.ts'
 
-const { MONGOURI } =  config()
+
+const existsEnv = await exists('./.env')
+
+let MONGOURI 
+if (existsEnv) {
+  MONGOURI =  config().MONGOURI
+} else {
+  MONGOURI = 'mongodb://localhost:27017'
+}
 const client = new MongoClient()
 client.connectWithUri(MONGOURI)
 const db = client.database('todos')
--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import { Application, Context } from 'https://deno.land/x/oak/mod.ts'
 import router from './routes.ts'
 import { config } from 'https://deno.land/x/dotenv/mod.ts'
+import { exists } from 'https://deno.land/std@0.51.0/fs/mod.ts'
 
 const app = new Application()
+const existsEnv = await exists('./.env')
+let PORT 
+if (existsEnv) {
+  PORT =  config().PORT
+} else {
+  PORT = '1337'
+}
 
-const { PORT } = config()
 app.use(router.routes())
 console.log(`
   Simple Todo List API
